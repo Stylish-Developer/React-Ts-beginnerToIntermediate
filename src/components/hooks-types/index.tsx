99598bfb50718eb2d05be831c0b694612c3d7ea8
gowtham <gowthamsgobi@gmail.com>
2023-06-21 15:16:16 +0530

    types for useState hook is workouted

---
+++ src/components/hooks-types/index.tsx
@@ -0,0 +1,13 @@
+import UsestateHook from "./Usestate";
+
+// -- Hooks Types
+const HooksTypes = () => {
+    return(
+        <>
+        <h2>Hooks Types</h2>
+        <UsestateHook />
+        </>
+    )
+}
+
+export default HooksTypes;
