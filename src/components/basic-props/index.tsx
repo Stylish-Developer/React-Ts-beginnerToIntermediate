b8fbf01b30d86ec31949f90810e52cceb422b299
gowtham <gowthamsgobi@gmail.com>
2023-06-23 17:47:01 +0530

    extracting-component-props workouted

--- src/components/basic-props/index.tsx
+++ src/components/basic-props/index.tsx
@@ -3,35 +3,40 @@ import BasicTypeProps from "./BasicProps";
 
 // -- BASIC TYPESCRIPT PROPS
 const BasicProps = () => {
-    // -- for boolean & number type concept
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null); // -- BONUS STEPS Don't worry about it here, we'll talk about it later
+  // -- for boolean & number type concept
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null); // -- BONUS STEPS Don't worry about it here, we'll talk about it later
 
-    setTimeout(() => {
-        setIsLoggedIn(!isLoggedIn)
-    }, 3000);
+  setTimeout(() => {
+    setIsLoggedIn(!isLoggedIn);
+  }, 3000);
 
-    // -- object type concept
-    const Myname = {
-        firstName: 'GAUTHAM',
-        lastName: "SAMPATHKUMAR"
-    }
+  // -- object type concept
+  const Myname = {
+    firstName: "GAUTHAM",
+    lastName: "SAMPATHKUMAR",
+  };
 
-    const names = [
-        { name: 'Jin', age: 25 },
-        { name: 'Eddy', age: 50 },
-        { name: 'Yosimizu', age: 30 },
-    ]
+  const names = [
+    { name: "Jin", age: 25 },
+    { name: "Eddy", age: 50 },
+    { name: "Yosimizu", age: 30 },
+  ];
 
-    useEffect(() => {
-        setIsLoggedIn(false);
-    }, []);
+  useEffect(() => {
+    setIsLoggedIn(false);
+  }, []);
 
-    return (
-        <>
-            <h2>Basic type props</h2>
-            <BasicTypeProps notification={20} isLoggedIn={isLoggedIn} name={Myname} names={names} />
-        </>
-    )
-}
+  return (
+    <>
+      <h2>Basic type props</h2>
+      <BasicTypeProps
+        notification={20}
+        isLoggedIn={isLoggedIn}
+        name={Myname}
+        names={names}
+      />
+    </>
+  );
+};
 
 export default BasicProps;
