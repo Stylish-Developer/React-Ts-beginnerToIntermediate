090f5777d22ad16829e42595cb7a74579e9ceee2
gowtham <gowthamsgobi@gmail.com>
2023-06-23 17:14:58 +0530

    wrapping html  elements concept workouted

---
+++ src/components/wrapping-html/index.tsx
@@ -0,0 +1,13 @@
+import WrappingElement from "./WrappingHtml";
+
+// -- Wrapping HTML Elements
+const WrappingHtml = () => {
+  return (
+    <>
+      <h3>Wrapping HTML Elments</h3>
+      <WrappingElement />
+    </>
+  );
+};
+
+export default WrappingHtml;
